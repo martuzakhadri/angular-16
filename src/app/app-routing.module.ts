2cfd703f24e4710f60fdb215715cbceca95b0638
martuza <martuzakhadri12@gmail.com+>
2023-08-18 00:24:27 +0530

    changes are added wrt backend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostsCreateComponentComponent } from './posts/posts-create-component/posts-create-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {path:'',component:PostListComponent},
+   {path:'create',component:PostsCreateComponentComponent},
+   {path:'edit/:postId',component:PostsCreateComponentComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
